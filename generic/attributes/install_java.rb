c0bf3840a143e360b626b667e4884e1d2399de9f
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-07 02:56:56 +0300

    Create install_java.rb

---
+++ generic/attributes/install_java.rb
@@ -0,0 +1,6 @@
+default['java']['jdk_version'] = '8'
+default['java']['remove_deprecated_packages'] = true
+default['java']['oracle']['accept_oracle_download_terms'] = true
+# default['java']['java_home'] = '/usr/lib/java/default'
+default['java']['set_default'] = true
+default['java_ark']['connect_timeout'] = 1200
