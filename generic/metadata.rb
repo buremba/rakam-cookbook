3796da9d2ee903c0969c87bc9eaed98751ffb951
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-07 03:50:47 +0300

    Delete metadata.rb

--- generic/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'rakam'
-maintainer       'Burak Emre Kabakci'
-maintainer_email 'emrekabakci@gmail.com'
-license          'all_rights'
-description      'Rakam Cluster Commons Cookbook'
-version          '0.1.0'
-
-depends 'java'
