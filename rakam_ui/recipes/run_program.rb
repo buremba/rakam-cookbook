bbb1e6207d6fb48f0c41bb1887b62139e565a1e6
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-07 19:17:18 +0300

    Update run_program.rb

--- rakam_ui/recipes/run_program.rb
+++ rakam_ui/recipes/run_program.rb
@@ -22,8 +22,11 @@ bash "download and build package" do
   EOH
 end
 
-include_recipe "rakam_ui::service"
-
-service "rakam_ui" do
-  action [ :enable, :start ]
+bash "run program" do
+  user "webapp"
+  cwd "home/webapp"
+  environment ({ 'HOME' => ::Dir.home("webapp"), 'USER' => "webapp" })
+  code <<-EOH
+    cd /home/webapp && nohup java -Dhttp.server.address=0.0.0.0:5000 -Dlog.levels-file=log.properties -Dlog.output-file=logs/app.log -Dlog.enable-console=false -cp rakam/rakam/target/dependency/*: org.rakam.ServiceStarter config.properties &
+  EOH
 end
