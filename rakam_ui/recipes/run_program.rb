9266684e6520fd253bda2092d340266e4dcf6383
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-07 13:56:44 +0300

    Update run_program.rb

--- rakam_ui/recipes/run_program.rb
+++ rakam_ui/recipes/run_program.rb
@@ -1,7 +1,21 @@
-bash "install-codedeploy-agent" do
+template "/var/www" do
+  source "config.properties.erb"
+end
+
+template "/var/www" do
+  source "log.properties.erb"
+end
+
+bash "download and build package" do
   code <<-EOH
     cd /var/www
     git clone git@github.com:buremba/rakam.git
     cd rakam && mvn clean install -DskipTests -Pbundled-with-ui -Pmove-package-to-dependency
+  EOH
+end
+
+bash "run program" do
+  code <<-EOH
+    nohup java -Dhttp.server.address=0.0.0.0:5000 -Dlog.levels-file=log.properties -Dlog.output-file=logs/app.log -Dlog.enable-console=false -cp rakam/target/dependency/*: org.rakam.ServiceStarter config.properties &
   EOH
 end
