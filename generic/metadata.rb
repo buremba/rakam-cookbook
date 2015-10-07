66126d1f82187be5b2f18f34228248b0d18555a7
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-07 03:24:33 +0300

    Create metadata.rb

---
+++ generic/metadata.rb
@@ -0,0 +1,8 @@
+name             'rakam'
+maintainer       'Burak Emre Kabakci'
+maintainer_email 'emrekabakci@gmail.com'
+license          'all_rights'
+description      'Rakam Cluster Commons Cookbook'
+version          '0.1.0'
+
+depends 'java'
