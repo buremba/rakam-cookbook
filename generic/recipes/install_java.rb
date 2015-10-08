e02949a3342a350fdde703cfa04850702bc5ae30
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-08 17:18:28 +0300

    Update install_java.rb

--- generic/recipes/install_java.rb
+++ generic/recipes/install_java.rb
@@ -5,7 +5,16 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe 'java::oracle'
+include_recipe 'ark::default'
 
+ark 'maven' do
+  url             'http://apache.mirrors.tds.net/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz'
+  checksum        '8c190264bdf591ff9f1268dc0ad940a2726f9e958e367716a09b8aaa7e74a755'
+  home_dir        '/usr/local/maven'
+  win_install_dir '/usr/local/maven'
+  version         '3.2.5'
+  append_env_path true
+end
 
 bash "create application user" do
   code <<-EOH
