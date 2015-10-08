5244d2b42e9953d76f52f35f7509cd1a2285331e
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-08 23:04:36 +0300

    Update run_program.rb

--- rakam_ui/recipes/run_program.rb
+++ rakam_ui/recipes/run_program.rb
@@ -32,7 +32,6 @@ end
 
 bash "run program" do
   code <<-EOH
-    cd /home/webapp/rakam
-    su webapp -l -c 'nohup java -Dhttp.server.address=0.0.0.0:5000 -Dui.directory=../rakam-ui/app -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp rakam/target/dependency/*: org.rakam.ServiceStarter ../config.properties &'
+    su webapp -l -c 'cd /home/webapp/rakam; nohup java -Dhttp.server.address=0.0.0.0:5000 -Dui.directory=../rakam-ui/app -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp rakam/target/dependency/*: org.rakam.ServiceStarter ../config.properties &'
   EOH
 end
