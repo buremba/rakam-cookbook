9d4389b6f4291b08f2512e5454cf1cce95370104
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-08 14:22:19 +0300

    Create metadata.rb

---
+++ rakam_ui/metadata.rb
@@ -0,0 +1,8 @@
+name             'rakam_ui'
+maintainer       'Burak Emre Kabakci'
+maintainer_email 'emrekabakci@gmail.com'
+license          'all_rights'
+description      'Rakam Cluster Cookbook'
+version          '0.1.0'
+
+depends 'nodejs'
