4abff35e8f414fb1a8336a38cc305451206b95e3
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-08 18:16:50 +0300

    Update run_program.rb

--- rakam_ui/recipes/run_program.rb
+++ rakam_ui/recipes/run_program.rb
@@ -16,26 +16,23 @@ include_recipe "nodejs::npm"
 
 bash "download and build package" do
   code <<-EOH
-    su webapp
     cd /home/webapp
-    if cd rakam; then git pull; else git clone https://github.com/buremba/rakam.git && cd rakam; fi
-    mvn clean install -DskipTests -Pmove-package-to-dependency
+    su webapp -l -c 'if cd rakam; then git pull; else git clone https://github.com/buremba/rakam.git && cd rakam; fi'
+    su webapp -l -c 'mvn clean install -DskipTests -Pmove-package-to-dependency'
   EOH
 end
 
 bash "download and build ui" do
   code <<-EOH
-    su webapp
     cd /home/webapp
-    if cd rakam-ui; then git pull; else git clone https://github.com/buremba/rakam-ui.git && cd rakam-ui; fi
-    npm install
+    su webapp -l -c 'if cd rakam-ui; then git pull; else git clone https://github.com/buremba/rakam-ui.git && cd rakam-ui; fi'
+    su webapp 'npm install'
   EOH
 end
 
 bash "run program" do
   code <<-EOH
-    su webapp
     cd /home/webapp
-    nohup java -Dhttp.server.address=0.0.0.0:5000 -Dui.directory=../rakam-ui -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp rakam/target/dependency/*: org.rakam.ServiceStarter ../config.properties &
+    su root -l -c 'nohup java -Dhttp.server.address=0.0.0.0:5000 -Dui.directory=../rakam-ui -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp rakam/target/dependency/*: org.rakam.ServiceStarter ../config.properties &'
   EOH
 end
