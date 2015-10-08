a0822c7d330acce53f373bd38923af2bfbc0106b
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-10-08 17:44:50 +0300

    Update run_program.rb

--- rakam_ui/recipes/run_program.rb
+++ rakam_ui/recipes/run_program.rb
@@ -20,6 +20,13 @@ bash "download and build package" do
     cd /home/webapp
     if cd rakam; then git pull; else git clone https://github.com/buremba/rakam.git && cd rakam; fi
     mvn clean install -DskipTests -Pmove-package-to-dependency
+  EOH
+end
+
+bash "download and build ui" do
+  code <<-EOH
+    su webapp
+    cd /home/webapp
     if cd rakam-ui; then git pull; else git clone https://github.com/buremba/rakam-ui.git && cd rakam-ui; fi
     npm install
   EOH
@@ -29,6 +36,6 @@ bash "run program" do
   code <<-EOH
     su webapp
     cd /home/webapp
-    cd /home/webapp/rakam && nohup java -Dhttp.server.address=0.0.0.0:5000 -Dui.directory=../rakam-ui -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp rakam/target/dependency/*: org.rakam.ServiceStarter ../config.properties &
+    nohup java -Dhttp.server.address=0.0.0.0:5000 -Dui.directory=../rakam-ui -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp rakam/target/dependency/*: org.rakam.ServiceStarter ../config.properties &
   EOH
 end
