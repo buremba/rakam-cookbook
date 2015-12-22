63bd8161a948e8fa759ce80c4f5880b96910e7c5
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-12-22 19:56:28 +0200

    Delete metadata.rb

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'rakam'
-maintainer       'Burak Emre Kabakcı'
-maintainer_email 'emrekabakci@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures rakam'
-long_description 'Installs/Configures rakam'
-version          '0.1.0'
