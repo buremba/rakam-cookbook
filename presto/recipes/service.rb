9a79cf1ae41651b6276574e4bab0b7126cb33e51
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-12-28 00:34:45 +0200

    Update service.rb

--- presto/recipes/service.rb
+++ presto/recipes/service.rb
@@ -1,7 +1,7 @@
 service "presto" do
   supports :status => true, :stop => true, :start => true
-  start_command "cd /home/webapp/presto && bin/launcher start"
-  stop_command "cd /home/webapp/presto && bin/launcher stop"
-  status_command "cd /home/webapp/presto && bin/launcher status"
+  start_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher start'"
+  stop_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher stop'"
+  status_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher status'"
   action :nothing
 end
