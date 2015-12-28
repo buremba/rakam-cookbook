39a57bf02d241adff8ef39a9203afcfae946c0e0
Burak Emre Kabakcı <emrekabakci@gmail.com>
2015-12-28 02:31:28 +0200

    Update start.rb

--- rakam/recipes/start.rb
+++ rakam/recipes/start.rb
@@ -2,6 +2,6 @@ include_recipe 'rakam::update_ui'
 
 bash "run program" do
   code <<-EOH
-    su webapp -l -c 'cd /home/webapp/rakam; nohup java -Dhttp.server.address=0.0.0.0:5000 -Dui.directory=../rakam-ui/app -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp rakam/target/rakam-*/lib/*: org.rakam.ServiceStarter ../config.properties &'
+    su webapp -l -c 'cd /home/webapp/rakam; nohup java -Dui.directory=../rakam-ui/app -Dlog.levels-file=../log.properties -Dlog.output-file=../logs/app.log -Dlog.enable-console=false -cp $(echo rakam/target/rakam-*-bundle/rakam-*/lib/)*: org.rakam.ServiceStarter ../config.properties &'
   EOH
 end
