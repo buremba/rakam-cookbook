6a42fb7c8fd87016c5edb6c8892a3deb78f2869f
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-02-27 15:45:31 +0200

    Update stop.rb

--- rakam/recipes/stop.rb
+++ rakam/recipes/stop.rb
@@ -1,5 +1,5 @@
 bash "stop program" do
   code <<-EOH
-    su webapp -l -c 'cd /home/webapp/rakam-server; bin/launcher stop'
+    su webapp -l -c 'test -d "/home/webapp/rakam-server" && cd /home/webapp/rakam-server; bin/launcher stop || echo First Install'
   EOH
 end
