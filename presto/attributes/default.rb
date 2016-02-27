1a1897d4257bac13dbaefab4a2a48cab5200c982
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-02-27 22:19:30 +0200

    Update default.rb

--- presto/attributes/default.rb
+++ presto/attributes/default.rb
@@ -5,3 +5,9 @@ default['data-dir'] = '/home/webapp/presto'
 default['rakam_streaming_checkout'] = 'HEAD'
 default['rakam_raptor_checkout'] = 'HEAD'
 default['jvm-properties'] = ''
+
+
+default['s3']['streaming'] = 'rakam-streaming'
+default['s3']['bulk'] = 'rakam-bulk'
+default['s3']['batch'] = 'rakam-batch'
+default['kinesis'] = 'rakam-stream'
