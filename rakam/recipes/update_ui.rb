8ae1942a5737f6b72fdf738bf78d8d9a45678ecd
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-04-21 21:23:22 +0300

    Update update_ui.rb

--- rakam/recipes/update_ui.rb
+++ rakam/recipes/update_ui.rb
@@ -1,7 +1,7 @@
 bash "download and build ui" do
   code <<-EOH
     cd /home/webapp
-    su webapp -l -c 'if cd rakam-ui; then git pull; else git clone https://github.com/buremba/rakam-ui.git && cd rakam-ui; fi'
+    su webapp -l -c 'if cd rakam-ui; then ssh-agent bash -c "ssh-add /home/webapp/.ssh/rakam_ui; git pull"; else ssh-agent bash -c "ssh-add /home/webapp/.ssh/rakam_ui; git clone git@github.com:buremba/rakam-ui.git" && cd rakam-ui; fi'
     su webapp -l -c 'cd rakam-ui; npm install' 
   EOH
 end
