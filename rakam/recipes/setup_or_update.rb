7f5c4576716480de80c83367fcb9ea07c17b49b1
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-05-23 02:50:08 +0300

    Update setup_or_update.rb

--- rakam/recipes/setup_or_update.rb
+++ rakam/recipes/setup_or_update.rb
@@ -8,19 +8,19 @@ if defined?(node['rakam-config']['ui.enable']) && node['rakam-config']['ui.enabl
     group "webapp"
     mode 0600
   end
+  
+  include_recipe "rakam::update_ui"
+else
+  log "Ignoring BI module"
+end
 
-  bash "download and build package" do
+bash "download and build package" do
   code <<-EOH
     cd /home/webapp
     su webapp -l -c 'if cd rakam; then git pull; else git clone https://github.com/buremba/rakam.git && cd rakam; fi; git checkout #{node['checkout']}'
     su webapp -l -c 'cd rakam; mvn clean install -DskipTests && rm -rf ../rakam-server && mv rakam/target/*-bundle/rakam-* ../rakam-server'
   EOH
-  end
-  
-else
-  log "Ignoring BI module"
 end
-
 
 template "/home/webapp/rakam-server/etc/config.properties" do
   source "config_#{node['deployment_type']}.properties.erb"
@@ -50,5 +50,3 @@ template "/home/webapp/rakam-server/etc/jvm.config" do
   group "webapp"
   mode 0644
 end
-
-include_recipe "rakam::update_ui"
