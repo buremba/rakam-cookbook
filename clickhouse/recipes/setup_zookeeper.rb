2681248c7ab72e6ac7b1ea634ceea09ee608ab5f
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-06-27 17:44:07 +0300

    remove zk dependency

--- clickhouse/recipes/setup_zookeeper.rb
+++ clickhouse/recipes/setup_zookeeper.rb
@@ -1,11 +1,6 @@
-zookeeper '3.4.8'
-
 template "/opt/zookeeper/zookeeper.cfg" do
   source "zookeeper.cfg.erb"
   owner "zookeeper"
   group "zookeeper"
   mode 0755
 end
-
-zookeeper_config '/opt/zookeeper/zookeeper.cfg'
-zookeeper_service 'zookeeper'
