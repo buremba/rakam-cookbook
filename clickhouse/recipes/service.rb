f74531404f0cf4e4d534f5dca438e6ea4ad36334
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-07-12 18:09:05 +0300

    fix click house start command

--- clickhouse/recipes/service.rb
+++ clickhouse/recipes/service.rb
@@ -1,6 +1,6 @@
 service "clickhouse" do
   supports :status => true, :stop => true, :start => true
-  start_command "ulimit -n 262144 && sudo service clickhouse-server start"
+  start_command "sudo service clickhouse-server start"
   stop_command "sudo service clickhouse-server stop"
   status_command "sudo service clickhouse-server status"
   action :nothing
