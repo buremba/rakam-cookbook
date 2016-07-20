bbbfa1fab3c4627cf19489245c5b9c8a59f33724
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-07-20 22:34:25 +0300

    use metrika user

--- clickhouse/recipes/setup_or_update.rb
+++ clickhouse/recipes/setup_or_update.rb
@@ -4,44 +4,43 @@ bash "download-and-setup-clickhouse" do
     echo "deb http://repo.yandex.ru/clickhouse/trusty/ dists/stable/main/binary-amd64/" | tee /etc/apt/sources.list.d/clickhouse.list && \
     apt-get update && \
     apt-get install --allow-unauthenticated -y clickhouse-server-common clickhouse-client && \
-    chown -R metrika /etc/clickhouse-server/
-    sudo apt-get install jq
+    chown -R metrika /etc/clickhouse-server/ && mkdir -p /home/metrika/ && chown -R metrika /home/metrika/ && sudo apt-get install jq 
   EOH
 end
 
 bash "download and worker" do
   code <<-EOH
-    cd /home/webapp
-    su webapp -l -c 'if cd rakam-clickhouse-worker; then git pull; else git clone https://github.com/buremba/rakam-clickhouse-worker.git && cd rakam-clickhouse-worker; fi; git checkout #{node['checkout']}'
-    su webapp -l -c 'cd rakam-clickhouse-worker; mvn clean install -DskipTests && rm -rf ../worker/lib && if [ -d '../worker' ] ; then mv target/*-bundle/clickhouse-worker-*/lib ../worker/lib; else mv target/*-bundle/clickhouse-worker-*/ ../worker; fi;'
+    cd /home/metrika
+    su metrika -l -c 'if cd rakam-clickhouse-worker; then git pull; else git clone https://github.com/buremba/rakam-clickhouse-worker.git && cd rakam-clickhouse-worker; fi; git checkout #{node['checkout']}'
+    su metrika -l -c 'cd rakam-clickhouse-worker; mvn clean install -DskipTests && rm -rf ../worker/lib && if [ -d '../worker' ] ; then mv target/*-bundle/clickhouse-worker-*/lib ../worker/lib; else mv target/*-bundle/clickhouse-worker-*/ ../worker; fi;'
   EOH
 end
 
-template "/home/webapp/worker/etc/config.properties" do
+template "/home/metrika/worker/etc/config.properties" do
   source "config_worker.properties.erb"
-  owner "webapp"
-  group "webapp"
+  owner "metrika"
+  group "metrika"
   mode 0755
 end
 
-template "/home/webapp/worker/etc/logging.properties" do
+template "/home/metrika/worker/etc/logging.properties" do
   source "logging.properties.erb"
-  owner "webapp"
-  group "webapp"
-  variables ({ :version => `bash -c "cd /home/webapp/rakam-clickhouse-worker; git describe --tags"` })
+  owner "metrika"
+  group "metrika"
+  variables ({ :version => `bash -c "cd /home/metrika/rakam-clickhouse-worker; git describe --tags"` })
   mode 0755
 end
 
-template "/home/webapp/worker/etc/jvm.config" do
+template "/home/metrika/worker/etc/jvm.config" do
   source "jvm.config.erb"
-  owner "webapp"
-  group "webapp"
+  owner "metrika"
+  group "metrika"
   mode 0755
 end
 
 bash "run worker" do
   code <<-EOH
-    su webapp -l -c 'cd /home/webapp/worker; bin/launcher restart'
+    su metrika -l -c 'cd /home/metrika/worker; bin/launcher restart'
   EOH
 end
 
