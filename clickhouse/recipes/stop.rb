2abde4e5a1927aea1f4580437e0971c125c76ad7
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-07-22 03:43:53 +0300

    stop worker

--- clickhouse/recipes/stop.rb
+++ clickhouse/recipes/stop.rb
@@ -1,3 +1,9 @@
 service 'clickhouse-server' do
   action :stop
 end
+
+bash "run worker" do
+  code <<-EOH
+    curl --output /dev/null --silent --head --fail http://127.0.0.1:8123 && su metrika -l -c 'cd /home/metrika/worker; bin/launcher stop'
+  EOH
+end
