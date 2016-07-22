5cd8706723a12520e33c87ec1fe2975a7f1ebf1c
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-07-22 04:06:42 +0300

    fix bug

--- clickhouse/recipes/stop.rb
+++ clickhouse/recipes/stop.rb
@@ -4,6 +4,6 @@ end
 
 bash "run worker" do
   code <<-EOH
-    curl --output /dev/null --silent --head --fail http://127.0.0.1:8123 && su metrika -l -c 'cd /home/metrika/worker; bin/launcher stop'
+    su metrika -l -c 'cd /home/metrika/worker; bin/launcher stop'
   EOH
 end
