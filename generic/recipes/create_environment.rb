74727ebc2c309700a61a0c97dbf7d3db62859be0
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-08-03 13:28:39 +0300

    update maven

--- generic/recipes/create_environment.rb
+++ generic/recipes/create_environment.rb
@@ -9,11 +9,11 @@ end
 include_recipe 'ark::default'
 
 ark 'maven' do
-  url             'http://apache.mirrors.tds.net/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz'
+  url             'http://apache.mirrors.tds.net/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz'
   checksum        '8c190264bdf591ff9f1268dc0ad940a2726f9e958e367716a09b8aaa7e74a755'
   home_dir        '/usr/local/maven'
   win_install_dir '/usr/local/maven'
-  version         '3.2.5'
+  version         '3.3.9'
   append_env_path true
 end
 
