5d11d51c6b3230ec47c760f10b43be1718bb132f
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-08-19 02:46:38 +0300

    remove wait_coordinator

--- presto/recipes/service.rb
+++ presto/recipes/service.rb
@@ -1,7 +1,9 @@
 service "presto" do
-  supports :status => true, :stop => true, :start => true
+  supports :status => true, :stop => true, :start => true, :restart => true
+  init_command "echo 'init'"
   start_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher start'"
   stop_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher stop'"
+  restart_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher restart'"
   status_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher status'"
   action :nothing
 end
--- presto/recipes/wait_coordinator.rb
+++
@@ -1,5 +0,0 @@
-bash "wait until coordinator becomes visible" do
-  code <<-EOH
-    for i in $(seq 600 $END); do if sudo opsworks-agent-cli get_json | jq '.opsworks.layers.presto.instances.presto1' | grep -q "id"; then break; else sleep 1; fi done;
-  EOH
-end
