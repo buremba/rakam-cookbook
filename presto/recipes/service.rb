a752588a89350a537d70a73b235df2f91e55567d
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-08-19 03:02:26 +0300

    update commands

--- presto/recipes/service.rb
+++ presto/recipes/service.rb
@@ -1,6 +1,6 @@
 service "presto" do
   supports :status => true, :stop => true, :start => true, :restart => true
-  init_command "echo 'init'"
+  init_command "/home/webapp/presto"
   start_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher start'"
   stop_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher stop'"
   restart_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher restart'"
