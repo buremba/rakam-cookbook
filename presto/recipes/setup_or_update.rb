fa8334d918ea85a8bf5ed4a22d77aad9658959fe
Burak Emre Kabakcı <emrekabakci@gmail.com>
2016-10-24 04:07:55 +0300

    cleanup

--- presto/recipes/setup_or_update.rb
+++ presto/recipes/setup_or_update.rb
@@ -88,18 +88,6 @@ template "/home/webapp/presto/etc/catalog/rakam_raptor.properties" do
   mode 0755
 end
 
-
-if node['modules'].include?('user')
-  template "/home/webapp/presto/etc/catalog/user.properties" do
-    source "catalog/user.properties.erb"
-    owner "webapp"
-    group "webapp"
-    mode 0755
-  end
-else
-  log "Ignoring User module"
-end
-
 template "/home/webapp/presto/etc/catalog/streaming.properties" do
   source "catalog/streaming.properties.erb"
   owner "webapp"
