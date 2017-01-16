b6e6bcdaaf2a99c6837ca2665708d2ef62557afb
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-01-17 00:42:54 +0300

    fix dir

--- presto/recipes/restart.rb
+++ presto/recipes/restart.rb
@@ -1,5 +1,5 @@
 bash "run program" do
   code <<-EOH
-    su webapp -l -c 'cd /home/webapp/presto && (bin/launcher restart) && (killall -9 presto-server-heartbeat || true) && (./presto-server-heartbeat &)'
+    su webapp -l -c 'cd /home/webapp/presto && (bin/launcher restart) && (killall -9 presto-server-heartbeat || true) && (../presto-server-heartbeat &)'
   EOH
 end
