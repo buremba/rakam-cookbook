890b527ae9d5720da369b9b915ee3ae7599d7129
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-01-17 00:00:01 +0300

    fix for killall

--- presto/recipes/start.rb
+++ presto/recipes/start.rb
@@ -1,5 +1,5 @@
 bash "run program" do
   code <<-EOH
-    su webapp -l -c 'cd /home/webapp/presto && (bin/launcher start) && (./presto-server-heartbeat &)'
+    su webapp -l -c 'cd /home/webapp/presto && (bin/launcher start) && (killall -9 presto-server-heartbeat || true) && (./presto-server-heartbeat &)'
   EOH
 end
