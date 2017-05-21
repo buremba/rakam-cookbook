d6b416396366ad25405e18779d1591aefc399e2a
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-05-21 20:57:46 +0300

    upgrade java

--- generic/attributes/install_java.rb
+++ generic/attributes/install_java.rb
@@ -2,6 +2,10 @@ default['java']['jdk_version'] = '8'
 default['java']['remove_deprecated_packages'] = true
 default['java']['oracle']['accept_oracle_download_terms'] = true
 # default['java']['java_home'] = '/usr/lib/java/default'
+
+default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131'
+default['java']['jdk']['8']['x86_64']['checksum'] = 'd97f0f402bd65a9c26aa266246b0894c8d6762e82373377641ca779c46406299'
+
 default['java']['set_default'] = true
 default['java_ark']['connect_timeout'] = 1200
 default['maven']['install_java'] = false
