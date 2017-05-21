ce06df5f4119681dd10b6641bb68bdf53837b3d7
Burak Emre Kabakcı <emrekabakci@gmail.com>
2017-05-21 21:03:48 +0300

    change checksum

--- generic/attributes/install_java.rb
+++ generic/attributes/install_java.rb
@@ -3,8 +3,8 @@ default['java']['remove_deprecated_packages'] = true
 default['java']['oracle']['accept_oracle_download_terms'] = true
 # default['java']['java_home'] = '/usr/lib/java/default'
 
-default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131'
-default['java']['jdk']['8']['x86_64']['checksum'] = 'd97f0f402bd65a9c26aa266246b0894c8d6762e82373377641ca779c46406299'
+default['java']['jdk']['8']['x86_64']['url'] = 'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = '62b215bdfb48bace523723cdbb2157c665e6a25429c73828a32f00e587301236'
 
 default['java']['set_default'] = true
 default['java_ark']['connect_timeout'] = 1200
