dbd476e0a214946281fb74239b41716e139a08dc
Burak Emre Kabakcı <emrekabakci@gmail.com>
2018-04-05 22:45:30 +0300

    Update stop.rb

--- rakam/recipes/stop.rb
+++ rakam/recipes/stop.rb
@@ -1,5 +1,5 @@
 bash "stop program" do
   code <<-EOH
-    su webapp -l -c '(test -d "/home/webapp/rakam-server" && (cd /home/webapp/rakam-server; bin/launcher stop)) || true'
+    su webapp -l -c 'if [ -d '/home/webapp/rakam-server' ]; then (/home/webapp/rakam-server/bin/launcher stop) && (killall -9 rakam-server-heartbeat || true); fi'
   EOH
 end
