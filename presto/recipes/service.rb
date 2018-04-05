160d5e953b176a7cb6fd2a87a592536eb8e99918
Burak Emre Kabakcı <emrekabakci@gmail.com>
2018-04-05 22:46:47 +0300

    Delete service.rb

--- presto/recipes/service.rb
+++
@@ -1,9 +0,0 @@
-service "presto" do
-  supports :status => true, :stop => true, :start => true, :restart => true
-  init_command "/home/webapp/presto"
-  start_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher start'"
-  stop_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher stop'"
-  restart_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher restart'"
-  status_command "su webapp -l -c 'cd /home/webapp/presto && bin/launcher status'"
-  action :nothing
-end
